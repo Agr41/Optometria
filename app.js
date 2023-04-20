7cc9f7acf5a1dd0958464307de3df2b239f80876
AaronGuapo1 <mraaronrodriguez@hotmail.com>
2023-04-20 10:06:57 -0600

    base de datos

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
 
 mongoose.set("strictQuery", true);
 mongoose.connect(
-    "mongodb://localhost:27017/Optometria",
+    "mongodb+srv://NoLeDeboANadie:rickygei@noledeboanadie.i6p3wc9.mongodb.net/test",
     { useNewUrlParser: true }
 );
 
