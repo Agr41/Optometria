1b5b47e3b0dc3420ea761f6e52506be6c2dde0bb
Mauricio Palomares <mau.palo2@gmail.com>
2023-07-18 09:25:49 -0600

    Proteger página configuración

--- routes/HomeSessions.js
+++ routes/HomeSessions.js
@@ -1,22 +1,5 @@
 var express = require('express');
 var router = express.Router();
-//var passport = require('passport');
-//var {client,dbName} = require('../db/mongo');
-
-// passport.deserializeUser(
-//   async function(id, done) {
-//     await client.connect();
-//     const db = client.db(dbName);
-//     const collection = db.collection('users');
-//     collection.findOne({username:id}, function (err, user) {
-//       done(err, user);});
-// });
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('HomeSessions', { title: 'HomeS',Logeado,role });
-// });
-
 
 router.get('/',(req, res, next) => {
   
--- routes/configuracion.js
+++ routes/configuracion.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('configuracion', { title: 'Configuración' });
+router.get('/',(req, res, next) => {
+  
+  if (req.session.userId!=null) {
+    res.render('configuracion', { title: 'Configuracion'});
+  } else {
+      res.redirect('/LoginInicio')
+  }
 });
 
 module.exports = router;
