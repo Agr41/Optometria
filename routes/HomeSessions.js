5ea42342f105fa97c9bb729a7a98e11a2d312917
Mauricio Palomares <mau.palo2@gmail.com>
2023-07-18 09:22:19 -0600

    Proteger página HomeSessions

--- routes/HomeSessions.js
+++ routes/HomeSessions.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
+//var passport = require('passport');
+//var {client,dbName} = require('../db/mongo');
 
+// passport.deserializeUser(
+//   async function(id, done) {
+//     await client.connect();
+//     const db = client.db(dbName);
+//     const collection = db.collection('users');
+//     collection.findOne({username:id}, function (err, user) {
+//       done(err, user);});
+// });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('HomeSessions', { title: 'HomeS',Logeado,role });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('HomeSessions', { title: 'HomeS',Logeado,role });
+// });
 
+
+router.get('/',(req, res, next) => {
+  
+  if (req.session.userId!=null) {
+    res.render('HomeSessions', { title: 'HomeS',Logeado,role });
+  } else {
+      res.redirect('/LoginInicio')
+  }
+});
 module.exports = router;
