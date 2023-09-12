14b40f1ce6870005388e37e388c2d462f32a0219
Ricardo García Alvarado <ricardogalvar@gmail.com>
2023-09-12 10:03:27 -0600

    redirecciones

--- routes/form.js
+++ routes/form.js
@@ -2,39 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/',(req, res, next) => {
-  
-  if (req.session.userId!=null) {
-    res.render('form', { title: 'Form' ,Logeado,role});
+router.get('/', (req, res, next) => {
+  if (req.session.userId != null) {
+    res.render('form', { title: 'Form', Logeado, role });
   } else {
-      res.redirect('/LoginInicio')
+    res.redirect('/LoginInicio');
   }
 });
 
-const paciente = require('../models/pacientes')
-router.post('/registrarpaciente',async function (req,res, next){
-    console.log("entró a post")
-if (req.session.userId!=null){
-  try{
-    await paciente.create(req.body)
-  res.send(`<script>alert("Se ha registrado al paciente")
-        window.location.href='/HomeSessions';
-        </script>`);
-        console.log("Registro correcto");}
-        catch{
-          res.send(`<script>alert("Hubo un error")
-        window.location.href='/HomeSessions';
-        </script>`);
-        }
-}
-else{
-  res.send(`<script>alert("Hubo un error")
-        window.location.href='/';
-        </script>`);
-}
-      
-      res.redirect('/HomeSessions')
+const paciente = require('../models/pacientes');
 
-}
-)
+router.post('/registrarpaciente', async function (req, res, next) {
+  console.log("entró a post");
+  if (req.session.userId != null) {
+    try {
+      await paciente.create(req.body);
+      // Mostrar una alerta de éxito y redirigir a
+      res.send('<script>alert("Se ha registrado al paciente"); window.location.href="https://www.tiktok.com/@pubear_cdc/video/7229458395072351493";</script>');
+      console.log("Hiciste el registro correctamente");
+    } catch (error) {
+      // Mostrar una alerta de error y redirigir a
+      res.send('<script>alert("Hubo un error al registrar al paciente"); window.location.href="https://www.tiktok.com/@pubear_cdc/video/7229458395072351493";</script>');
+    }
+  } else {
+    // Mostrar una alerta de error y redirigir a
+    res.send('<script>alert("Hubo un error al registrar al paciente"); window.location.href="https://www.tiktok.com/@pubear_cdc/video/7229458395072351493";</script>');
+  }
+});
+
 module.exports = router;
