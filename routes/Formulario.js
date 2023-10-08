79fe2ea449194eacf7f124386a1f9d0d4b2b93c8
AaronGuapo1 <mraaronrodriguez@hotmail.com>
2023-10-08 01:16:51 -0600

    tests

--- routes/Formulario.js
+++ routes/Formulario.js
@@ -14,6 +14,17 @@ module.exports = async (req, res) => {
   req.body.FechaDelTest = date;
 
   try {
+    if (req.body.opcion=== '+1')
+    {
+      req.body.ODReni =  req.body.ODReniPlus1
+      req.body.OIReni =  req.body.OIReniPlus1
+
+    }
+    if (req.body.opcion=== '+2'){
+      req.body.ODReni =  req.body.ODReniPlus2
+      req.body.OIReni =  req.body.OIReniPlus2
+    }
+    console.log(req.body);
     await Test.create(req.body);
     // Mostrar una alerta de éxito y redirigir a '/HomeSessions'
     res.send('<script>alert("Se guardaron los datos del paciente correctamente"); window.location.href="/HomeSessions";</script>');
--- routes/form.js
+++ routes/form.js
@@ -16,9 +16,33 @@ router.post('/registrarpaciente', async function (req, res, next) {
   console.log("entró a post");
   if (req.session.userId != null) {
     try {
+
+
+
+
+
+let contador = await paciente.countDocuments();
+
+function generarId() {
+  contador++; // Aumentar el contador en 1 en cada llamada
+  if (contador > 999999) {
+    // Reiniciar el contador si llega a 999999
+    contador = 1;
+  }
+
+  const id = contador.toString().padStart(6, '0'); // Convertir el contador en una cadena de 6 dígitos
+
+  return id;
+}
+
+      
+      // Ejemplo de uso:
+      const nuevoId = generarId();
+      req.body.Folio = nuevoId
       await paciente.create(req.body);
+
       // Mostrar una alerta de éxito y redirigir a
-      res.send('<script>alert("Se ha registrado al paciente"); window.location.href="/form";</script>');
+      res.send(`<script>alert("Se ha registrado al paciente. Número de fólio: ${nuevoId}"); window.location.href="/form";</script>`);
       console.log("Hiciste el registro correctamente");
     } catch (error) {
       // Mostrar una alerta de error y redirigir a
