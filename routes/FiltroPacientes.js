5e5ac324609a5ac9a28d4298c795708816d7ef0b
AaronGuapo1 <mraaronrodriguez@hotmail.com>
2023-10-09 09:12:38 -0600

    filtros

--- routes/FiltroPacientes.js
+++ routes/FiltroPacientes.js
@@ -1,5 +1,5 @@
 const Paciente = require("../models/pacientes.js")
-
+const Test = require("../models/Tets.js")
 module.exports = async (req, res) => {
     var page = req.query.page;
     var Filtro = req.body.Tipo;
@@ -30,6 +30,58 @@ module.exports = async (req, res) => {
             res.render('PanelPacientes',{pacientes,Logeado, role, FiltroPaginado,Filtro,consulta,TotalPaginas})
 
         }
+        if(Filtro ==="HIPERMETROPIA"){
+          const tests = await Test.find({$or: [{ ODReni: "HIPERMETROPIA"},{ OIReni: "HIPERMETROPIA"} ]});
+          const testIds = tests.map(test => test.id);
+          
+          const pacientes = await Paciente.paginate({ _id: { $in: testIds } }, { page: 1, limit: 10 });
+          
+          const FiltroPaginado = true;
+          const consulta = req.body.busqueda;
+          var TotalPaginas = [];
+          for (i = 0; i < pacientes.totalPages; i++) {
+            TotalPaginas.push(i + 1);
+          }
+    
+          res.render('PanelPacientes',{pacientes,Logeado, role, FiltroPaginado,Filtro,consulta,TotalPaginas})
+
+      }
+    
+      if(Filtro ==="EMETROPE"){
+        const tests = await Test.find({$or: [{ ODReni: "EMETROPE" },{ OIReni: "EMETROPE" }]});
+        const testIds = tests.map(test => test.id);
+        
+        const pacientes = await Paciente.paginate({ _id: { $in: testIds } }, { page: 1, limit: 10 });
+        const FiltroPaginado = true;
+        const consulta = req.body.busqueda;
+        var TotalPaginas = [];
+        for (i = 0; i < pacientes.totalPages; i++) {
+          TotalPaginas.push(i + 1);
+        }
+  console.log(pacientes)
+        res.render('PanelPacientes',{pacientes,Logeado, role, FiltroPaginado,Filtro,consulta,TotalPaginas})
+
+    }
+    if(Filtro ==="MIOPIA"){
+      const tests = await Test.find({
+        $or: [
+          { ODReni: "MIOPIA" },
+          { OIReni: "MIOPIA" }
+        ]
+      });
+            const testIds = tests.map(test => test.id);
+      
+      const pacientes = await Paciente.paginate({ _id: { $in: testIds } }, { page: 1, limit: 10 });      
+      const FiltroPaginado = true;
+      const consulta = req.body.busqueda;
+      var TotalPaginas = [];
+      for (i = 0; i < pacientes.totalPages; i++) {
+        TotalPaginas.push(i + 1);
+      }
+
+      res.render('PanelPacientes',{pacientes,Logeado, role, FiltroPaginado,Filtro,consulta,TotalPaginas})
+
+  }
 
 
     }else if (page != undefined){
