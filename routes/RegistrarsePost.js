fca39312c865b3e0ed795618bb59abba7c1e02c8
Mauricio Palomares Herrera <mau.palo2@gmail.com>
2023-11-03 16:20:04 -0600

    Redirige a HomeSessions al crear usuario

--- routes/RegistrarsePost.js
+++ routes/RegistrarsePost.js
@@ -2,6 +2,6 @@ const User = require('../models/Usuarios')
 module.exports = async (req,res)=>{
     await User.create(req.body)
     await User.updateOne({username:req.body.username},{username:req.body.username})
-    res.redirect('/')
+    res.redirect('/HomeSessions')
     //Checar que sea admin
 }
