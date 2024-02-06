56fe39202b3dc524148ab3cc5adcfe4a368d080a
unknown <luispg2010@hotmail.com>
2024-02-06 15:48:32 -0600

    Pruebas Complementarias

---
+++ routes/pruebas_complementarias.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const Paciente = require('../models/pacientes')
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  if (req.session.userId!=null) {
+    const pacientes = await Paciente.find({})
+  //console.log(usuarios)
+  res.render('pruebas_complementarias', { title: 'pruebas complementarias',Logeado,role,pacientes, usuario });
+  } else {
+      res.redirect('/LoginInicio')
+  }
+  
+});
+
+module.exports = router;
