18fe949e0ca9e73a3f31533dd51365ae5c319b08
AaronRdz69 <aaron.rodriguez@metricamovil.com>
2024-02-21 12:35:28 -0600

    aa

--- routes/cambiarIdioma.js
+++ routes/cambiarIdioma.js
@@ -2,5 +2,4 @@ const User = require('../models/Usuarios')
 module.exports = async (req,res)=>{
     await User.updateOne({username:req.body.username},{language:req.body.language})
     res.redirect('/HomeSessions')
-    //Checar que sea admin
 }
