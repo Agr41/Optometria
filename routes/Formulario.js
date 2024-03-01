748025e14513adafc9ea7daff720776f3a000e34
Mauricio Palomares Herrera <mau.palo2@gmail.com>
2024-03-01 09:27:48 -0600

    Añadir botones en todos los formularios

--- routes/Formulario.js
+++ routes/Formulario.js
@@ -16,45 +16,67 @@ module.exports = async (req, res) => {
   req.body.FechaDelTest = date;
 
   try {
-    if (req.body.opcion=== '+1' && req.body.ODAsigmatismo1 !== 'on')
-    {
-      req.body.ODReni =  req.body.ODReniPlus1
-      req.body.OIReni =  req.body.OIReniPlus1
+    if (req.body.opcion === '+1' && req.body.ODAsigmatismo1 !== 'on') {
+      req.body.ODReni = req.body.ODReniPlus1
+      req.body.OIReni = req.body.OIReniPlus1
 
     }
-    if (req.body.opcion=== '+2'  && req.body.ODAsigmatismo1 !== 'on'){
-      req.body.ODReni =  req.body.ODReniPlus2
-      req.body.OIReni =  req.body.OIReniPlus2
+    if (req.body.opcion === '+2' && req.body.ODAsigmatismo1 !== 'on') {
+      req.body.ODReni = req.body.ODReniPlus2
+      req.body.OIReni = req.body.OIReniPlus2
     }
 
-    if (req.body.opcion=== '+1' && req.body.ODAsigmatismo1 === 'on' )
-    {
-      req.body.ODReni = 'Astigmatismo' +' '+ req.body.ODReniPlus1
+    if (req.body.opcion === '+1' && req.body.ODAsigmatismo1 === 'on') {
+      req.body.ODReni = 'Astigmatismo' + ' ' + req.body.ODReniPlus1
 
     }
-    if (req.body.opcion=== '+1' && req.body.OIAsigmatismo1 === 'on' )
-    {
-      req.body.OIReni =  'Astigmatismo' +' '+ req.body.OIReniPlus1
+    if (req.body.opcion === '+1' && req.body.OIAsigmatismo1 === 'on') {
+      req.body.OIReni = 'Astigmatismo' + ' ' + req.body.OIReniPlus1
 
     }
-    if (req.body.opcion=== '+2' && req.body.ODAsigmatismo2 === 'on'){
-      req.body.ODReni =  'Astigmatismo' +' '+ req.body.ODReniPlus2
+    if (req.body.opcion === '+2' && req.body.ODAsigmatismo2 === 'on') {
+      req.body.ODReni = 'Astigmatismo' + ' ' + req.body.ODReniPlus2
     }
-    if (req.body.opcion=== '+2' && req.body.OIAsigmatismo2 === 'on'){
-      req.body.OIReni =  'Astigmatismo' +' '+ req.body.OIReniPlus2
+    if (req.body.opcion === '+2' && req.body.OIAsigmatismo2 === 'on') {
+      req.body.OIReni = 'Astigmatismo' + ' ' + req.body.OIReniPlus2
     }
 
 
     //console.log(req.body);
     await Test.create(req.body);
-    
+
     // Mostrar una alerta de éxito y redirigir a '/HomeSessions'
-    res.send('<script>alert("Se guardaron los datos del paciente correctamente"); window.location.href="/HomeSessions";</script>');
+    switch (req.body.selectorPagina) {
+      case "salir":
+        res.send(`<script>alert("Se guardaron los datos del paciente"); window.location.href="/HomeSessions";</script>`);
+        break;
+      case "preeliminares":
+        res.send(`<script>alert("Se guardaron los datos del paciente"); window.location.href="/preeliminares";</script>`);
+        break;
+      case "salud_ocular":
+        res.send(`<script>alert("Se guardaron los datos del paciente"); window.location.href="/SaludOcular";</script>`);
+        break;
+      case "pruebas_f":
+        res.send(`<script>alert("Se guardaron los datos del paciente"); window.location.href="/pruebas_f";</script>`);
+        break;
+      case "pruebas_c":
+        res.send(`<script>alert("Se guardaron los datos del paciente"); window.location.href="/pruebas_complementarias";</script>`);
+        break;
+      case "rx_final":
+        res.send(`<script>alert("Se guardaron los datos del paciente"); window.location.href="/rx_final";</script>`);
+        break;
+      case "clinica":
+        res.send(`<script>alert("Se guardaron los datos del paciente"); window.location.href="/menu_clinica";</script>`);
+        break;
+      default:
+        res.send(`<script>alert("Se guardaron los datos del paciente"); window.location.href="/HomeSessions";</script>`);
+        break;
+    }
   } catch (error) {
-      console.error(error);
+    console.error(error);
     // Mostrar una alerta de error y redirigir a '/HomeSessions'
     res.send('<script>alert("Hubo un error al guardar los datos del paciente"); window.location.href="/HomeSessions";</script>');
 
-    
+
   }
 };
