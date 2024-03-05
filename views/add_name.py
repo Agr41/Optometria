5c401f19cbd38291a37ca5665823f5cf95f1ef19
Mauricio Palomares Herrera <mau.palo2@gmail.com>
2024-03-05 11:11:04 -0600

    Completar salud ocular

--- views/add_name.py
+++ views/add_name.py
@@ -18,5 +18,5 @@ def add_name_to_input_tags(html_file):
     with open(html_file, 'w') as file:
         file.write(str(soup))
 
-html_file = 'pruebas_f.hbs'
+html_file = 'SaludOcular.hbs'
 add_name_to_input_tags(html_file)
